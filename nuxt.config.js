d28392ca05b953924daa271185beb49591535224
Ovi Savescu <ovi@ovswebsites.com>
2018-12-13 19:06:08 +0200

    added helpers plugin (was filters)

--- nuxt.config.js
+++ nuxt.config.js
@@ -41,7 +41,7 @@ module.exports = {
   /*
   ** Plugins to load before mounting the App
   */
-  plugins: ['~plugins/components.js'],
+  plugins: ['~plugins/components', '~plugins/helper.js'],
 
   /*
   ** Nuxt.js modules 
