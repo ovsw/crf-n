c1331b978fdae718df7ad046ab79131bc67a053a
Ovi Savescu <ovi@ovswebsites.com>
2018-12-15 06:23:43 +0200

    basic routes

--- pages/_section/_page/index.vue
+++ pages/_section/_page/index.vue
@@ -15,13 +15,17 @@ export default {
   //   return { story: { content: { heading: '' } } }
   // },
   asyncData(context) {
+    let endpoint = `cdn/stories/${context.params.section}/${
+      context.params.page
+    }`
+
     // Check if we are in the editor mode
     let version =
       context.query._storyblok || context.isDev ? 'draft' : 'published'
 
     // Load the JSON from the API
     return context.app.$storyapi
-      .get('cdn/stories/home', {
+      .get(endpoint, {
         version: version
       })
       .then(res => {
