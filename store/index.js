08a221e49e1c3b65344a20603506ac001ffcedbe
Ovi Savescu <ovi@ovswebsites.com>
2018-12-16 18:18:56 +0200

    get all links into store

--- store/index.js
+++ store/index.js
@@ -6,17 +6,30 @@ const createStore = () => {
       cacheVersion: '',
       settings: {
         main_navi: []
-      }
+      },
+      mainNavTree: {}
     },
     mutations: {
       setSettings(state, settings) {
         state.settings = settings
+      },
+      setMainNavTree(state, mainNavTree) {
+        state.mainNavTree = mainNavTree
       },
       setCacheVersion(state, version) {
         state.cacheVersion = version
       }
     },
     actions: {
+      nuxtServerInit({ commit }, context) {
+        return this.$storyapi
+          .get(`cdn/links`, {
+            version: context.version
+          })
+          .then(res => {
+            commit('setMainNavTree', res.data.links)
+          })
+      },
       loadSettings({ commit }, context) {
         return this.$storyapi
           .get(`cdn/stories/settings`, {
